fcdbe7b9e8b42d4d7d741c83ff1dee5eccc285d1
Sayed <s.rouhani7979@gmail.com>
2024-04-26 21:17:27 +0330

    Import files ui

--- PL-language/PL-language/Form1.Designer.cs
+++ PL-language/PL-language/Form1.Designer.cs
@@ -28,12 +28,127 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            button2 = new Button();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            richTextBox1 = new RichTextBox();
+            richTextBox2 = new RichTextBox();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            tabPage2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(24, 21);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 1;
+            button1.Text = "import";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(168, 21);
+            button2.Name = "button2";
+            button2.Size = new Size(152, 34);
+            button2.TabIndex = 2;
+            button2.Text = "lexical analyzer";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.BackColor = SystemColors.AppWorkspace;
+            flowLayoutPanel1.Dock = DockStyle.Bottom;
+            flowLayoutPanel1.Location = new Point(0, 585);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(1202, 60);
+            flowLayoutPanel1.TabIndex = 3;
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Dock = DockStyle.Bottom;
+            tabControl1.Location = new Point(0, 89);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(1202, 496);
+            tabControl1.TabIndex = 4;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(richTextBox1);
+            tabPage1.Location = new Point(4, 34);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(1194, 458);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "input";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(richTextBox2);
+            tabPage2.Location = new Point(4, 34);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(1194, 458);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "output";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Dock = DockStyle.Fill;
+            richTextBox1.Font = new Font("Segoe UI", 12F);
+            richTextBox1.Location = new Point(3, 3);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(1188, 452);
+            richTextBox1.TabIndex = 0;
+            richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            richTextBox2.Dock = DockStyle.Fill;
+            richTextBox2.Location = new Point(3, 3);
+            richTextBox2.Name = "richTextBox2";
+            richTextBox2.Size = new Size(1188, 452);
+            richTextBox2.TabIndex = 0;
+            richTextBox2.Text = "";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1202, 645);
+            Controls.Add(tabControl1);
+            Controls.Add(flowLayoutPanel1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage2.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
+        private Button button1;
+        private Button button2;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private RichTextBox richTextBox1;
+        private TabPage tabPage2;
+        private RichTextBox richTextBox2;
     }
 }
--- PL-language/PL-language/Form1.cs
+++ PL-language/PL-language/Form1.cs
@@ -1,3 +1,5 @@
+using System.Windows.Forms;
+
 namespace PL_language
 {
     public partial class Form1 : Form
@@ -5,6 +7,32 @@ namespace PL_language
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "open file";
+            ofd.Filter = "txt files (*.txt)|*.txt|c files(*.c)|*.c|All files (*.*)|*.*";
+
+            string fileAddress = "";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                fileAddress = ofd.FileName;
+            }
+            string fileContain = File.ReadAllText(fileAddress);
+            richTextBox1.Text = fileContain;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
