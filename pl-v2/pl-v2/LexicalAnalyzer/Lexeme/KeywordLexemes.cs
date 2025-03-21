f40c4e4bd19b275d55c754c5cd7edf3558bb8d1b
yekobist <iopty1984@gmail.com>
2025-03-21 03:48:14 -0700

    Add all keywords lexems

--- pl-v2/pl-v2/LexicalAnalyzer/Lexeme/KeywordLexemes.cs
+++ pl-v2/pl-v2/LexicalAnalyzer/Lexeme/KeywordLexemes.cs
@@ -10,7 +10,18 @@ namespace pl_v2.LexicalAnalyzer.Lexeme
     {
         internal KeywordLexemes() 
         {
-         Lexemes.Add()
+            Lexemes.Add(new Lexeme("bool", "T_Bool"));
+            Lexemes.Add(new Lexeme("break", "T_Break"));
+            Lexemes.Add(new Lexeme("char", "T_Char"));
+            Lexemes.Add(new Lexeme("continue", "T_Continue"));
+            Lexemes.Add(new Lexeme("else", "T_Else"));
+            Lexemes.Add(new Lexeme("false", "T_False"));
+            Lexemes.Add(new Lexeme("for", "T_For"));
+            Lexemes.Add(new Lexeme("if", "T_If"));
+            Lexemes.Add(new Lexeme("int", "T_Int"));
+            Lexemes.Add(new Lexeme("print", "T_Print"));
+            Lexemes.Add(new Lexeme("return", "T_Return"));
+            Lexemes.Add(new Lexeme("true", "T_True"));
         }
         internal List<Lexeme> Lexemes = new List<Lexeme> ();
     }
